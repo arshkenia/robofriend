2d6e6b1666d95e4af3a687e92aee374693b53ebf
Arsh Kenia <arshkenia@gmail.com>
2020-08-20 17:58:21 +0530

    Updates

---
+++ precache-manifest.7cb8dcc137ebba106bf493a2347ab240.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "263ff32f6300c60626a673da1dd9ed71",
+    "url": "/robofriend/index.html"
+  },
+  {
+    "revision": "97d18fd6cf8b9f035ffd",
+    "url": "/robofriend/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "e7de9ed678cce0e17f30",
+    "url": "/robofriend/static/css/main.f3e59d68.chunk.css"
+  },
+  {
+    "revision": "97d18fd6cf8b9f035ffd",
+    "url": "/robofriend/static/js/2.526585eb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriend/static/js/2.526585eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e7de9ed678cce0e17f30",
+    "url": "/robofriend/static/js/main.9db83b4a.chunk.js"
+  },
+  {
+    "revision": "b9549992ed9aa0bec102",
+    "url": "/robofriend/static/js/runtime-main.b099a5de.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriend/static/media/SEGA.12a3db8b.woff"
+  }
+]);
